3d7109b2eb89917c160cb68990ee39dde373a4c9
Neil Ding <bbbear444@gmail.com>
2015-02-01 19:22:39 -0500

    add extension icon and auto fill form

--- content_scripts/utilities/form_populator/controller.js
+++ content_scripts/utilities/form_populator/controller.js
@@ -1,24 +1,70 @@
-// $('body').append('<div id="populator" class="jj-button">populate</div>');
+(function(window, $) {
+  window.jj = window.jj || {};
+  var jj = window.jj;
+  var FILL_TRY_INTERVAL = 500;
+  var FILL_TRY_TIMES = 10;
 
+  $.fn.setFieldValue = function(value) {
+    var $this   = $(this);
+    var tagName = $this.prop("tagName");
 
-$('#populator').on('click', function (populator) {
-  $('input').each(function () {
-    var $this    = $(this)
-      , title    = $this.attr('title') || ''
-      , value    = titleValueMap[title.toLowerCase()]
-      , tabEvent = jQuery.Event("keydown");
+    switch (tagName) {
+      case 'INPUT':
+      case 'TEXTAREA':
+        return $this.val(value);
 
-    if ($this.is('input')) {
-      var $label = $this.closest('.row').find('label');
+      case 'SELECT':
+        return $this.find('option').each(function() {
+          var $option = $(this);
+          if ($option.val() === value) {
+            $option.prop('selected', true);
+          }
+        });
 
-      $this.attr('value', value);
-      $label.addClass('field-has-value');
+      default:
+        return false;
+    }
+  };
 
-      $this.trigger('focus')
-      $this.trigger('blur')
+  function popForm() {
+    var $forms  = $('form');
+    var $fields = $forms.find('input, select, textarea');
+    var counter = 0;
 
-    } else if ($this.is('select')) {
-      // TODO : select functionality
+    $fields.each(function() {
+      var $field = $(this);
+      var value  = jj.nameValueMap.get($field) || '';
+      // only fill empty field
+      if (value && !$field.val()) {
+        $field.setFieldValue(value);
+        counter++;
+      }
+    });
+
+    return !!counter;
+  }
+
+  // if auto-fill is checked, fill the form automatically
+  chrome.storage.sync.get('isAutoFillForm', function(item) {
+    var counter = 0;
+    var timer   = null;
+
+    if (item.isAutoFillForm) {
+      // try to fill the form for times with a interval
+      // do this because some checkout is rendering form
+      // dynamically without event firing.
+      (function tryToFill(){
+        timer = setTimeout(function() {
+          if (!jj.popForm() && counter < FILL_TRY_TIMES) {
+            console.log('trying ... ' + counter);
+            counter++;
+            tryToFill();
+          }
+        }, FILL_TRY_INTERVAL);
+      })();
     }
   });
-});
+
+  jj.popForm = popForm;
+
+})(window, $);
